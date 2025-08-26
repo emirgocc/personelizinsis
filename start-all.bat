ab762e3a5e2fe4a19590c9fb699278c320fc42d4
emirgocc <emirgoc_39@hotmail.com>
2025-08-26 14:56:43 +0300

    frontend build edilirken sorun çıkarttığı için kaldırıldı ayrı bir proje olarak pushlandı.

--- start-all.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Personel İzin Sistemi - Tümünü Başlat
-
-REM Backend'i yeni bir komut penceresinde başlat
-start cmd /k "cd /d %cd%\backend && C:\xampp\php\php.exe -S 0.0.0.0:8000 -t ."
-
-REM Frontend'i başlat
-cd frontend
-npm start
---
+++ start-backend.bat
@@ -0,0 +1,2 @@
+
+start cmd /k "cd /d %cd%\backend && C:\xampp\php\php.exe -S 0.0.0.0:8000 -t ."
